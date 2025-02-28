3a3f0b4ecc55b65784cf48d5826438241b6601d8
MatheusHenriqueNF <matheus.henrique26@unifesp.br>
2025-02-27 22:29:31 -0300

    Sprint 3 | Começo da tela Inicial

--- Sprint 3/toten-viasmart/src/app/layout.tsx
+++ Sprint 3/toten-viasmart/src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,11 +12,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="pt-br">
+      
+      <body>
+        <main className="w-full h-full">
+
+        
         {children}
+        </main>
       </body>
     </html>
   );
